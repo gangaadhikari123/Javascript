d996552bbc746f1de0ecf7bfeb23465a76066ec3
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-07-04 08:30:07 +0545

    some practices

---
+++ script.js
@@ -0,0 +1,40 @@
+/*console.log("hello World");
+let a=5;
+let b=2;
+console.log("a+b=",a+b);
+let num = prompt("Enter a number");
+
+if(num>=80)
+{
+    console.log("A");
+}
+else if(num>=70)
+{
+    console.log("B+");
+
+}*/
+for(let i=1;i<=5;i++){
+    console.log("I can Do!");
+}
+//to calculate n number sum
+let sum=0;
+for(let i=1; i<=5 ; i++){
+    sum=sum+i;
+}
+console.log("sum=",sum);
+console.log("loop has ended");
+let num=0;
+{
+    if(num=0;num<=100;num++)
+    console.log("num=",num);
+}
+for(let i=1; i<=5;i++){
+    console.log("Ganga Adhikari");
+}
+//TO calculate sum of n numbers
+let sum=0;
+let n=15;
+for (let i=1;i<=n;i++);{
+sum=sum+1;
+}
+console.log("sum=",sum);
