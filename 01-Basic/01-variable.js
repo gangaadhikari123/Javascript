450b4faa1fe1ade0c539f9c182c009030c973c50
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-08 20:06:45 +0545

    some basic concepts of datatypes

---
+++ 01-Basic/01-variable.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all Js code as newer version
+// alert( 3+3 ) // shows error (we are using node js not browser)
+
+console.log(6+5)
+ console.log( 3 +
+    5
+ );//no code readability
+
+
+ // data types
+ let name ="Ganesh" //string
+ let ag =12 //intrger
+ let isLoggedIn = false //boolean
+let state = null;
+
+ // number => 2
+ // to power 53
+
+ // null => standalone value
+
+// undefined => if value not defined
+ // symbol 
+
+ //ECMAScript for documentation
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" =>33
+// "33Aab" => NaN
+
+// true => 1; false => 0;
+
+let IsLoggedIn = 1
+let booleanIsLoggedIn 
