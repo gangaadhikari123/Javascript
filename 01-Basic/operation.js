83258f413fde814f46a030e2e41945be0274fb30
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-08 20:56:23 +0545

    operations in js

---
+++ 01-Basic/operation.js
@@ -0,0 +1,32 @@
+let value =3
+let negvalue = -value
+//console.log(negvalue);
+
+
+let str1 =" hello"
+let str2 =" Ruku"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log ("1" +2);//12
+
+console.log( 1 + "2");//12
+
+console.log ("1" +2 + 2);//122
+
+console.log( 1+ 2 +"2");//32
+
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3
+ 
+num1 = num2 =num3 = 2+ 2 
+
+let gameCountrer =100
+gameCountrer++;
+console.log(gameCountrer);// 101
+
+
