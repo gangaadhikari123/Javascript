8f419dfea83350dd64aaa8714147587befb73c38
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-09 06:48:19 +0545

    some avoiding comparision tequenies

---
+++ 01-Basic/comparsion.js
@@ -0,0 +1,25 @@
+// we have to avoid yhese types of comparision threse causes confusion
+
+
+
+
+console.log("2" >1);
+console.log("02" > 1);//true
+
+console.log(null>0);//false
+
+console.log(null== 0);//false
+
+console.log(null >= 0);//true
+
+console.log(undefined == 0);//false
+
+
+// ===
+//strict check
+console.log("2"===2);//false
+
+
+
+
+
