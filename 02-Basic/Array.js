30c7ede9e638b4af64ed7bee1d29114039234f4b
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-12 17:29:11 +0545

    Array and its methods

---
+++ 02-Basic/Array.js
@@ -0,0 +1,29 @@
+const myArr = [0,1,3,4,5];
+console.log(myArr);
+// data type can be any type
+
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
+console.log(myArr[4]);
+
+const myArr2 = new Array(2,3,4,5);
+console.log(myArr2);
+
+// Array methods
+//push is used to add elements at the end of the array
+//pop is used to remove elements from the end of the array
+//shift is used to remove element from the start of the array
+//unshift is used to add elements at the start of the array
+
+myArr.push(6)
+myArr.push(7)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.shift(0)
+console.log(myArr);
+
