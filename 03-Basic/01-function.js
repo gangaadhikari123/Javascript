f13bf31640d9a964c7ee583857f507b50366e4b5
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-15 21:19:39 +0545

    function

---
+++ 03-Basic/01-function.js
@@ -0,0 +1,14 @@
+function myFun(){
+
+    console.log("H");
+    console.log("e");
+    console.log("l");
+    console.log("l");
+    console.log("o");
+}
+//myFun()
+
+function addTwoNum (num1, num2){
+    console.log (num1 + num2);
+}
+addTwoNum(4,5)
