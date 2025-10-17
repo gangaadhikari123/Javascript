f6a2bc3fd4813418bf093518ea7270096401bed8
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-17 11:07:32 +0545

    function and values in function

--- 03-Basic/01-function.js
+++ 03-Basic/01-function.js
@@ -29,3 +29,31 @@ function loginUser(username){
 }
 console.log(loginUser());
 
+
+//rest operator
+
+function calCartPrice(...num1){
+
+
+    return num1
+}
+
+console.log(calCartPrice(200,300,400))
+
+//passing object
+
+const user ={
+    name : "fj",
+    price : 324
+}
+function handleObject(anyObj){
+    console.log(`Username is ${anyObj.name} and price is ${anyObj.price}`);
+}
+handleObject(user)
+
+const myNewArray = [200,499,4000]
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+console.log(returnSecondValue(myNewArray));
