3496714073d8a2faabd1869bb312a6567a64faf1
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-10-17 11:17:56 +0545

    basic scope

---
+++ 03-Basic/scope.js
@@ -0,0 +1,22 @@
+/*let a =10;
+const b= 20;
+var c= 40;
+*/
+//console.log(a,b,c);
+
+
+if (true){
+    let a =10;
+    /*{[
+    block scope]*/
+}
+const b= 20;
+var c= 40;
+
+
+//console.log(a);
+console.log(b);
+console.log(c);
+
+//blocked scope
+//global scope
