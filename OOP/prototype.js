a703fdd0d7c110093ae884424d81ac8b08c62db5
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-05 20:53:04 +0545

    prototype

---
+++ OOP/prototype.js
@@ -0,0 +1,32 @@
+let myname = " RUKU"
+let mychannel = "RUKU TECH"
+
+console.log(myname.length);// 5
+
+console.log(myname.truelength);// undefined
+
+let myheros= ["shaktiman","naagraj","doga"]
+
+let heropower = {
+    shaktiman : "flight",
+    naagraj : "snake powers",
+    doga : "strength",
+
+    getshaktiman : function(){
+        console.log(`super power is ${this.shaktiman}`);
+
+    },
+
+    getnaagrajpower : function(){
+        console.log(`super power is ${this.naagraj}`);
+
+    },
+
+    getdogapower : function(){
+        console.log(`super power is ${this.doga}`);
+
+    }
+
+}
+
+object.prototype.R
