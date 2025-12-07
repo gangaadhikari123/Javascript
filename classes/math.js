d45d3aa11494dd306cedaeac808085c492c3afd7
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-07 17:49:47 +0545

    Objects in js

--- classes/math.js
+++ classes/math.js
@@ -1,1 +1,31 @@
-Object.
+const descriptor = Object.getOwnPropertyDescriptor(Math,"PI");
+console.log(descriptor);
+
+
+
+
+// console.log(Math.PI);
+
+const chai = {
+    name: "ginger chai",
+    price: 200,
+    IsAvailable: true
+}
+// console.log(chai);
+
+console.log(Object.getOwnPropertyDescriptor(chai, "price"));
+
+Object.defineProperty(chai,"name",{
+    writable:false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+
+
+for( let [key, value] of Object.entries(chai)){
+    if(typeof value !== 'function'){
+        console.log(`${key}: ${value}`);
+    }
+}
